b6c4a4e87ca5ffc8ae2ef0ec7ee091b194bb385a
Gleb Batykov <gleb.batykov@gmail.com>
2022-11-27 23:02:45 +0300

    Updates CacheAgentData

--- lib/src/cache_agent_data.dart
+++ lib/src/cache_agent_data.dart
@@ -1,6 +1,12 @@
 part of minerva_cache;
 
 class CacheAgentData extends AgentData {
-  CacheAgentData({String name = 'cache', super.data})
-      : super(name, CacheAgent());
+  CacheAgentData(
+      {String name = 'cache',
+      bool deleteOnExpire = true,
+      Duration checkPeriod = const Duration(seconds: 600)})
+      : super(
+            name,
+            CacheAgent(
+                deleteOnExpire: deleteOnExpire, checkPeriod: checkPeriod));
 }
